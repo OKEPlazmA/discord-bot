d38fdb241066d7e2abdd537c06565ebef3147d34
the_kaseys <coprograming@gmail.com>
2016-11-07 20:28:49 -0700

    a couple things from kc

--- bot.js
+++ bot.js
@@ -1,1 +1,19 @@
-var Discord = require ('discord.js')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', function(message) {
+    var input = message.content.toUpperCase();
+
+    if (input === 'IS THAT FREE FOR BACKERS') {
+        message.reply('We repeated this way to much! This is making me mad. I am breaking the DRY principle!!! DO NOT MAKE ME CRASH');
+    }
+    if (input === 'IS IT FREE') {
+        message.reply('Do I look like a charity?');
+    }
+});
+
+client.login('the key');
