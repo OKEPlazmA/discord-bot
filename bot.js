4105275414717b03b802e69a594721b354a07a4d
Cassidy Carr <cassidycr94@gmail.com>
2016-11-14 00:16:09 -0600

    Fixed Code/Added Welcome Message
    Fixed spacing and added welcome message, also fixed redeem file.

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ var Discord = require('discord.js'),
     VerEx = require('verbal-expressions'),
     // The HTTP module
     http = require('http'),
-// to make https request e.g get,post,put,delete
+    // to make https request e.g get,post,put,delete
     request = require('request'),
     functionHelper = require('./functionHelpers.js');
 
@@ -16,50 +16,51 @@ bot.on('message', function(message) {
   var input = message.content.toUpperCase();
 
   //This part is where we define the conditions
-  var condition1 = input.includes("KICKSTARTER BACKER") && input.includes("COURSE") && input.includes("FREE");
-  var condition2 = input.includes("KICKSTARTER BACKER") && input.includes("FREE");
-  var condition3 = input.includes("KICKSTARTER BACKER") && input.includes("COURSE");
-  var condition4 = input.includes("KICKSTARTER") && input.includes("COURSE");
-  var condition5 = input.includes("BACKER") && input.includes("COURSE");
+  var kickstarterCondition1 = input.includes("KICKSTARTER BACKER") && input.includes("COURSE");
+  var kickstarterCondition2 = input.includes("KICKSTARTER") && input.includes("COURSE");
+  var kickstarterCondition3 = input.includes("BACKER") && input.includes("COURSE");
   var lateEvent = input.includes("LATE") && input.includes("PLEDGE");
 
-     // stackoverflow API
-   if (input.includes('?')) {
+  // stackoverflow API
+  if (input.includes('?')) {
        console.log("llego al ? ");
         if (input.includes(prefix)) {
             console.log("llego al ! excalamcion");
             functionHelper.stackOverflowApiResults(functionHelper.removeThatPhrase(message.content, prefix, ' '),message);
             }
    }
-   
-   
-  //Conditional responses
-  functionHelper.checkConditions([input === "I AM PRETTY" || input === "I AM PRETTY ?"],
-                                  message,
-                                  "Yes. You are always Pretty. Keep Smiling."); 
-  functionHelper.checkConditions([condition4, condition3, condition5],
-                                  message, 
-                                  "yes it's free for Kickstarter backer who pledge above $100");
-  functionHelper.checkConditions([lateEvent], 
-                                  message, 
-                                  "yeah email jason@devslope.com for more info");
-  functionHelper.checkConditions([input.includes("LOVING"), input.includes("LIKE") && input.includes("BOT")], 
-                                  message, 
-                                  "Thank you. You are way cooler than me");
-  functionHelper.checkConditions([input === "BOT WHO ARE YOU"], 
-                                  message, 
-                                  "I'm here to help you to become a better developer. I am a work in progress");
-  
 
-  
+  //Conditional responses
+  functionHelper.checkConditions([input === "I AM PRETTY" || input === "I AM PRETTY?" || input === "I AM PRETTY ?"],
+                                  message,
+                                  "Yes. You are always Pretty. Keep Smiling.");
+  functionHelper.checkConditions([input.includes("LOVING"), input.includes("LIKE") && input.includes("BOT")],
+                                  message,
+                                  "Thank you. You are way cooler than me.");
+  functionHelper.checkConditions([input === "BOT WHO ARE YOU"],
+                                  message,
+                                  "I'm here to help you to become a better developer. I am a work in progress.");
+  functionHelper.checkConditions([kickstarterCondition1, kickstarterCondition2, kickstarterCondition3],
+                                  message,
+                                  "Kickstarter backers who pledged above $100 get lifetime access for FREE to any and all courses that Devslopes will ever release.");
+  functionHelper.checkConditions([lateEvent],
+                                  message,
+                                  "Email jason@devslope.com for more info.");
+
+
   //Unconditional responses
   functionHelper.response(message);
-  
-  
+
+
   // This function check for !Course and !help and !Coupon
   functionHelper.messageAuthor(message,prefix);
 
 });
+
+//Welcomes new members
+bot.on("guildMemberAdd", (member) => {
+    member.guild.defaultChannel.sendMessage("Hello " + member.user + " Welcome to the Devslopes community!")
+  });
 
 //Login to Discord using oauth
 bot.login('MjQ1NjI0NzI4NDMyMTQ4NDgy.CwOzaQ.yB4TBGLmU9QMZcQrYt1aed3xZ20');
