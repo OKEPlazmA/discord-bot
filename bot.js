6a7d6cc3c4b1312b006d14fadb9786788d4d7a00
Cassidy Carr <cassidycr94@gmail.com>
2016-12-21 04:56:54 -0600

    update

--- bot.js
+++ bot.js
@@ -1,53 +1,21 @@
 var Discord = require('discord.js'),
-    stackexchange = require('stackexchange'),
     VerEx = require('verbal-expressions'),
-    search = require('youtube-search'),
-    // The HTTP module
     http = require('http'),
-    // to make https request e.g get,post,put,delete
     request = require('request'),
-    functionHelper = require('./functionHelpers.js'),
     apiai = require('apiai');
 
 // bot client You can now use the bots features
 var bot = new Discord.Client();
-const prefix = '!';
 
 // This will run whenever the bot get a message. / whenever a message is sent to a server that it is in
 bot.on('message', function(message) {
     let input = message.content.toUpperCase();
     let msgContent = message.content;
 
-    //prevent the bot from issuing commands
-    if (message.author.bot) {
-        return;
-    }
-
-    // Google Search Function
-    if ((msgContent).startsWith("!G")) {
-        console.log("input: " + input);
-        functionHelper.googleSearch(functionHelper.removeThatPhrase(message.content, "!G", ' '), message);
-    }
-
-    // stackoverflow API
-    if ((msgContent).startsWith("!S")) {
-        console.log("llego al ? ");
-        if (input.includes(prefix)) {
-            console.log("llego al ! excalamcion");
-            functionHelper.stackOverflowApiResults(functionHelper.removeThatPhrase(message.content, '!S', ' '), message);
-        }
-    }
-
-    // YouTube API
-    if ((msgContent).startsWith("!Y")) {
-        functionHelper.youtubeApiResults((functionHelper.removeThatPhrase(input, '!Y', '')), message);
-    }
-
-    //Unconditional responses
-    functionHelper.response(message);
-
-    // This function check for !Course and !help and !Coupon
-    functionHelper.messageAuthor(message, prefix);
+   //prevent the bot from issuing commands
+   if (message.author.bot) {
+       return;
+   }
 
 //API.AI START
 
@@ -62,33 +30,16 @@ bot.on('message', function(message) {
     // Log all responses.
     request.on('response', function(response) {
         let responseText = response.result.fulfillment.speech;
-        console.log(response);
         message.reply(`${responseText}`);
+        console.log(response);
     });
 
     request.on('error', function(error) {
         console.log(error);
     });
 
-
     request.end()
 
-        // if the message equals the prefix + something, run what's inside.
-       if (input.startsWith(prefix + "")) {
-
-           // Sends a request to discord with the message content. Also removes the prefix. Change the number depending on the length of your prefix.
-           app.textRequest(`${input}`);
-           // Get that response
-           request.on('response', function(response) {
-           // I did this to make it easier to read. Set's response text equal to the output speech.
-           let responseText = response.result.fulfillment.speech;
-           // Send the message to discord.
-           bot.sendMessage(msg, `${responseText}`);
-        });
-
-      }
-
-    // Pretty self explanitory but it logs errors.
     request.on('error', function(error) {
         console.log(error);
     });
